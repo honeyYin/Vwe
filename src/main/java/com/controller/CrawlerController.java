5bd9244dc157d59598851ab2041097ecf8a71422
hzyinhonglian <hzyinhonglian@corp.netease.com>
2016-06-01 20:39:33 +0800

    crawler

--- src/main/java/com/controller/CrawlerController.java
+++ src/main/java/com/controller/CrawlerController.java
@@ -1,10 +1,15 @@
 package com.controller;
 
+import java.io.IOException;
+import java.util.List;
+
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-import java.io.IOException;
+
+import com.google.common.collect.Lists;
+import com.model.LinkModel;
 
 /**
  * Example program to list links from a URL.
@@ -12,32 +17,51 @@ import java.io.IOException;
 public class CrawlerController {
     public static void main(String[] args) throws IOException {
         String url = "http://www.babytree.com/";
-        print("Fetching %s...", url);
-
-        Document doc = Jsoup.connect(url).get();
-        Elements links = doc.select("a[href]");
-        Elements media = doc.select("[src]");
-        Elements imports = doc.select("link[href]");
-
-        /*print("\nMedia: (%d)", media.size());
-        for (Element src : media) {
-            if (src.tagName().equals("img"))
-                print(" * %s: <%s> %sx%s (%s)",
-                        src.tagName(), src.attr("abs:src"), src.attr("width"), src.attr("height"),
-                        trim(src.attr("alt"), 20));
-            else
-                print(" * %s: <%s>", src.tagName(), src.attr("abs:src"));
-        }
-
-        print("\nImports: (%d)", imports.size());
-        for (Element link : imports) {
-            print(" * %s <%s> (%s)", link.tagName(),link.attr("abs:href"), link.attr("rel"));
-        }*/
-
-        print("\nLinks: (%d)", links.size());
-        for (Element link : links) {
-            print(" * a: <%s>  (%s)", link.attr("abs:href"), trim(link.text(), 35));
-        }
+        List<LinkModel> linkModels = getLinks(url,"宝宝");
+        print("size=[%s]", linkModels.size());
+    }
+    private static List<LinkModel>  getLinks(String url,String keyWord){
+    	List<LinkModel> results = Lists.newArrayList();
+    	print("Fetching %s...", url);
+		try{
+		        Document doc = Jsoup.connect(url).get();
+		        Elements links = doc.select("a[href]");
+		        print("\nLinks: (%d)", links.size());
+		        for (Element link : links) {
+		            print(" * a: <%s>  (%s)", link.attr("abs:href"), trim(link.text(), 35));
+		            if(link.text().contains("")){
+		            	LinkModel model = new LinkModel();
+			            model.setFromUrl(url);
+			            model.setTitle(trim(link.text(), 35));
+			            model.setUrl(link.attr("abs:href"));
+			            results.add(model);
+		            }
+		            
+		        }
+		        
+		        Elements media = doc.select("[src]");
+		        Elements imports = doc.select("link[href]");
+		
+		        /*print("\nMedia: (%d)", media.size());
+		        for (Element src : media) {
+		            if (src.tagName().equals("img"))
+		                print(" * %s: <%s> %sx%s (%s)",
+		                        src.tagName(), src.attr("abs:src"), src.attr("width"), src.attr("height"),
+		                        trim(src.attr("alt"), 20));
+		            else
+		                print(" * %s: <%s>", src.tagName(), src.attr("abs:src"));
+		        }
+		
+		        print("\nImports: (%d)", imports.size());
+		        for (Element link : imports) {
+		            print(" * %s <%s> (%s)", link.tagName(),link.attr("abs:href"), link.attr("rel"));
+		        }*/
+		
+		        
+		}catch(Exception e){
+			print("fail to crawler from url:%s", url);
+		}
+    	return results;
     }
     private static void print(String msg, Object... args) {
         System.out.println(String.format(msg, args));
---
+++ src/main/java/com/model/LinkModel.java
@@ -0,0 +1,16 @@
+package com.model;
+
+import lombok.Getter;
+
+import lombok.Setter;
+
+@Setter
+@Getter
+public class LinkModel {
+
+	private String title;
+	
+	private String url;
+	
+	private String fromUrl;
+}
