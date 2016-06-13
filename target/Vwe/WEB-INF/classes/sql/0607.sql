51279ad9f96dd17cebbc02c246d998d749969133
unknown <>
2016-06-13 20:22:29 +0800

    test

--- target/Vwe/WEB-INF/classes/sql/0607.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE Paper ADD COLUMN isDraft INT(1);
-ALTER TABLE Paper MODIFY COLUMN channelId bigint(20) NULL;
-UPDATE Paper set isDraft = 0;
--- target/classes/sql/0530.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE Paper ADD COLUMN isRecom INT(1);
-
-UPDATE Paper set isRecom = 0;
-
-update paper set pregStage = 0 where pregStage is NULL;
